1b68b7fadb390c6e929b055489e1bfd626122ab2
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2019-07-17 06:33:49 -0400

    latest updates

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/stylesheets/App.css';
-import { Home } from './components';
+import { Home, About } from './components';
 
 function App() {
   return (
     <div className='app'>
       <Home />
+      <About />
     </div>
   );
 }
